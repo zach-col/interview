8acfb2ca0cebb94eef3bc47c6b34fb6305d29eba
zach <theperfectpicture100@gmail.com>
2018-03-10 17:05:04 -0600

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,4 +1,6 @@
-question5.py
+#Currently trying to figure out how to make this work properly
+
+#question5.py
 
 #Find the element in a singly linked list that's m elements from the end. For example, if a linked list has 5 elements, the 3rd element from the end is the 3rd element. The function definition should look like question5(ll, m), where ll is the first node of a linked list and m is the "mth number from the end". You should copy/paste the Node class below to use as a representation of a node in the linked list. Return the value of the node at that position.
 
